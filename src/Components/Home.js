4df9d20b08609727a641669fdcd8d36f9d0d65d3
omkharche <omkharche321@gmail.com>
2023-06-08 02:45:46 +0530

    desktop view done

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -100,7 +100,7 @@ const btnprev = () => {
             
             <div>
               <div className='slider'>
-                <button className='moveleft' onClick={btnprev}><p><i class="fa-solid fa-chevron-left"></i></p></button>
+              <button className='moveleft' onClick={btnprev}><p><i class="fa-solid fa-chevron-left"></i></p></button>
                 <button className='moveright' onClick={btnnext}><p><i class="fa-solid fa-chevron-right"></i></p></button>
               
                 <br></br>
@@ -124,7 +124,7 @@ const btnprev = () => {
                 <p className='sm1'>. </p>
                 <div className='subbox'>
                   <div className='sb1'>   
-                    <p className='sf1'>Subscribe to our Newsletter!</p>
+                  <p className='sf1'>Subscribe to our Newsletter!</p>
                     <p className='sf2'>Get the latest updates and offers from our firm<br></br>
                     right in your email.</p>
                   </div>
@@ -133,16 +133,117 @@ const btnprev = () => {
                     <button className='btn12'>Submit</button>
                   </div>
                 </div>
+                <div className="footer-details container">
+                  <div className='row1'>
+                    
+                    <div className='co1'>
+                      <ul className='list1'>
+                        <li className='fh'>
+                          Quick Links
+                        </li>
+                        <li className='fd'>
+                          Services
+                        </li>
+                        <li className='fd'>
+                          Privacy Policy
+                        </li>
+                        <li className='fd'>
+                          Internships
+                        </li>
+                        <li className='fd'>
+                          Blogs
+                        </li>
+                      </ul>
+
+                    </div>
+                  </div>
+                  <div className='row1'>
+                    <div className='co1'>
+                      <ul className='list1'>
+                        <li className='fh'>
+                          Useful Links
+                        </li>
+                        <li className='fd'>
+                          Contact Us
+                        </li>
+                        <li className='fd'>
+                          About Us
+                        </li>
+                        <li className='fd'>
+                          Home
+                        </li>
+                        <li className='fd'>
+                          Career
+                        </li>
+                      </ul>
+
+                    </div>
+                  </div>
+                  <div className='row'>
+                    <div className='co1'>
+                      <ul className='list1'>
+                        <li className='fh'>
+                          Working Hours
+                        </li>
+                        <li className='fd'>
+                          10:30am – 8:00pm
+                        </li>
+                        <li className='fd'>
+                          Monday to Saturday
+                        </li>
+                        <li className='fd'>
+                          The Company is Closed on Sundays
+                        </li>
+                      </ul>
+
+                    </div>
+                  </div>
+                  <div className='row'>
+                    <div className='co1'>
+                      <ul className='list1'>
+                        <li className='fh'>
+                          Contact Us
+                        </li>
+                        <li className='fd'>
+                          +91-9021070048
+                        </li>
+                        <li className='fd'>
+                          info@cwtech.in
+                        </li>
+                        <li className='fd'>
+                          Hinjewadi Phase 3, Pune, India
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                  <div className='row'>
+                    <div className='co1'>
+                      <ul className='list1'>
+                        <li className='fh'>
+                          Follow
+                        </li>
+                        <li className='fd1'>
+                        <i class="fa-brands fa-instagram"></i>&nbsp;&nbsp;&nbsp;
+                        <i class="fa-brands fa-facebook"></i>
+                        </li>
+                        <li className='fd1'>
+                        <i class="fa-brands fa-twitter"></i>&nbsp;&nbsp;
+                        <i class="fa-brands fa-youtube"></i>
+                        </li>
+                        <li className='fd1'>
+                        <i class="fa-brands fa-linkedin-in"></i>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+                <div className='line'></div>
+                <p className='rights'>2023 All rights are reserved @CW_Technologies</p>
                 <br></br>
-                  <br></br>
-                  <br></br>
-                  <br></br>
-                  <br></br>
-                  <br></br>
               </div>
             </footer>
 
-      </>
+      </> 
   )
   
 }
